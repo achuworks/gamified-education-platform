8fc735797468335d2dea73f28e1fbdebfc89b5d5
PresidentL <missionuniverse07@gmail.com>
2025-02-06 12:27:02 +0530

    removed unwanted pakage

--- src/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-session-api' {
-    export class Session {
-      static set(key: string, value: any): void;
-      static get<T = any>(key: string): T | null;
-      static has(key: string): boolean;
-      static remove(key: string): void;
-    }
-  }
-  
